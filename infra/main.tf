5acd2542837679e79b1eae4a3af2207f78ee5fa9
bilalzardoa <zardoabilal@gmail.com>
2026-02-02 17:57:54 +0100

    iaM configurations

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,5 @@
+
+# ----- basic configurations -----
 # random provider is nodig voor random_id suffix te doen etc
 terraform {
   required_providers {
@@ -26,4 +28,39 @@ locals {
 # Random suffix om resource namen uniek te maken
 resource "random_id" "suffix" {
   byte_length = 4
+}
+
+
+# ----- IaM config -----
+
+# service accounts
+resource "google_service_account" "vertex_sa" {
+  account_id   = "vertex-sa"
+  display_name = "Vertex AI Service Account"
+}
+
+# permissions => (user/admin/viewer)
+
+resource "google_project_iam_member" "vertex_ai" {
+  project = var.project_id
+  role    = "roles/aiplatform.user"
+  member  = "serviceAccount:${google_service_account.vertex_sa.email}"
+}
+
+resource "google_project_iam_member" "storage" {
+  project = var.project_id
+  role    = "roles/storage.objectAdmin"
+  member  = "serviceAccount:${google_service_account.vertex_sa.email}"
+}
+
+resource "google_project_iam_member" "logging" {
+  project = var.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.vertex_sa.email}"
+}
+
+resource "google_project_iam_member" "monitoring" {
+  project = var.project_id
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.vertex_sa.email}"
 }
